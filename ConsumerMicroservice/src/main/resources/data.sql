ae3f8b83253daa939df47d0fa3a1444fce5226bb
saravananb15 <sb7097@srmist.edu.in>
2021-06-22 16:05:29 +0530

    code deployment

---
+++ ConsumerMicroservice/src/main/resources/data.sql
@@ -0,0 +1,74 @@
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Retailer','E-tailer', 90 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Retailer','Independent Grocer', 70 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Retailer','Home and Textile', 55 , 3);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Retailer','Pharmacy', 38 , 3);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Retailer','Mass Merchandiser', 52 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Health Practitioner','Medical Professional', 60 , 3);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Health Practitioner','Health Club', 65 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Distributor','Wholesaler of Finished Products', 33, 1);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Food Service','Full Service Restaurant', 91 , 4);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Food Service','Corporate Dining', 65 , 3);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Food Service','Coffee House', 54 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Food Service','Private Chef', 47 , 3);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Supplier','Equipment Supplier', 44 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Supplier','Ingredient Importer', 62 , 3);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Manufacturer','Food', 83 , 3);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Manufacturer','Pharmaceuticals', 68 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Manufacturer','Cosmetics', 34 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Manufacturer','Beverage', 86 , 4);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Manufacturer','Pet Products', 71 , 3);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Business Services','Consultant', 90 , 1);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Business Services','Government Agency', 57 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Business Services','Publisher', 61 , 3);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Business Services','Financial Institution', 58 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Investor','Private Equity Fund', 73 , 2);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Investor','Corporate Investor', 62 , 1);
+INSERT INTO Business_Master (Business_Category,Business_Type , Total_Employees , Business_age) VALUES  ('Investor','Hedge Fund', 64 , 3);
+
+
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Pay Back','Building','Owner', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Fire','Building','Owner', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Replacement','Building','Owner', 1);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Burglary','Building','Owner', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('All Risks','Building','Owner', 1);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Natural Disaster','Building','Owner', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Pay Back','Building','Rented', 1);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Fire','Building','Rented', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Burglary','Building','Rented', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Replacement','Building','Rented', 1);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Natural Disaster','Building','Rented', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Fire','Factory Equipment','Owner', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Burglary','Factory Equipment','Owner', 1);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Replacement','Factory Equipment','Owner', 1);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Pay Back','Factory Equipment','Owner', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('All Risks','Factory Equipment','Owner', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Natural Disaster','Factory Equipment','Owner', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Fire','Factory Equipment','Rented', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Burglary','Factory Equipment','Rented', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Replacement','Factory Equipment','Rented', 4);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Pay Back','Factory Equipment','Rented', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('All Risks','Factory Equipment','Rented', 4);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Natural Disaster','Factory Equipment','Rented',1);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Pay Back','Property in Transit','Owner', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Burglary','Property in Transit','Owner', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Natural Disaster','Property in Transit','Owner', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('All Risks','Property in Transit','Owner', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Replacement','Property in Transit','Owner', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Fire','Property in Transit','Owner', 1);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Pay Back','Property in Transit','Rented', 4);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Burglary','Property in Transit','Rented', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Natural Disaster','Property in Transit','Rented', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('All Risks','Property in Transit','Rented', 2);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Replacement','Property in Transit','Rented', 3);
+INSERT INTO Property_Master (Insurance_Type,Property_Type,Building_Type,Building_Age) VALUES ('Fire','Property in Transit','Rented', 2);
+
+
+
+
+
+
+
+
+
+
+
